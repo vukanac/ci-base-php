305be206f432468eacffb9912f3d605ce44ac409
Vladimir Vukanac <vukanac@gmail.com>
2018-05-28 20:04:56 +0200

    Fix failing CS - add namespace

--- src/Email.php
+++ src/Email.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+namespace App;
+
+use InvalidArgumentException;
+
 final class Email
 {
     private $email;
--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use App\Email;
 use PHPUnit\Framework\TestCase;
 
 final class EmailTest extends TestCase
